f912d3753a0699334f07e403bbbf9f6feda1e4ed
Gabriel Felix <gabriel.alves2208@gmail.com>
2025-02-21 19:07:11 -0300

    Criando componente App e Header

---
+++ src/screens/home/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1.5rem; // 24px
+  width: 100%;
+`;

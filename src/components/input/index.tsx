a144b7be52ce534d6191b332e0469a28ef0fccb0
Gabriel Felix <gabriel.alves2208@gmail.com>
2025-02-22 14:10:56 -0300

    Componente Input e Máscara.

---
+++ src/components/input/index.tsx
@@ -0,0 +1,17 @@
+import { type ComponentProps, forwardRef } from 'react';
+import { Container } from './styles';
+
+type InputProps = ComponentProps<'input'> & {
+  label?: string;
+  variant?: 'black' | 'dark';
+};
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, variant = 'black', ...props }, ref) => (
+    <Container $variant={variant}>
+      {/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
+      {label && <label>{label}</label>}
+      <input ref={ref} {...props} />
+    </Container>
+  ),
+);
---
+++ src/components/input/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import { theme } from '../../styles/theme';
+
+type ContainerProps = {
+  $variant: 'black' | 'dark';
+};
+
+export const Container = styled.div<ContainerProps>`
+  display: flex;
+  flex-direction: column;
+  gap: 0.125rem;  // 2px
+  width: 100%;  
+
+  label {
+    color: ${theme.colors.white};
+    font-size: 0.625rem;  // 10px
+    font-weight: 400;
+  }
+
+  input {
+    height: 2.25rem;  // 36px
+    background-color: ${(props) => theme.colors[props.$variant]};
+    border: 0;
+    border-radius: 0.25rem;  // 4px
+    padding: 0 0.75rem;  // 12px
+    color: ${theme.colors.neutral};
+    font-size: 1rem;  // 16px
+    width: 100%;
+    border: 1px solid transparent;
+    transition: all 0.2s ease-in;
+
+    &:focus {
+      border-color: ${theme.colors.primary};
+    }
+
+    &::placeholder {
+      color: ${theme.colors.neutral};
+    }
+  }
+`;
